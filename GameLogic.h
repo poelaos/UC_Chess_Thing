d0cd9802eec20919f76f317d6428f2ba0adbe32b
poelaos <95561446+poelaos@users.noreply.github.com>
2024-04-23 14:43:59 -0700

    Update GameLogic.h

--- GameLogic.h
+++ GameLogic.h
@@ -1,1 +1,23 @@
-oop
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
+
+#include <iostream>
+#include "GameState.h"
+
+void ticTacToe_Single (int tttInteger) {
+    gameInteger = tttInteger;
+    while (gameInteger == 1) {
+        GameState game;
+        int input1 = -1;
+        int input2 = -1;
+
+        while (game.done == false) {
+            std::cin >> input1;
+            std::cin >> input2;
+            game.play(input1,input2);
+
+        }
+    }
+}
+
