ccfd0caeddd3248744f44479f077cc7ef87ba800
AmanKalkat <110202775+AmanKalkat@users.noreply.github.com>
2024-04-26 16:50:51 -0700

    Update main.cpp
    - added switch cases for menu and game mode select

--- main.cpp
+++ main.cpp
@@ -5,11 +5,45 @@
 using namespace std;
 
 int main(){
-    GameState game;
 
-    game.play(0, 0);
-    game.play(1, 1);
-    cout << game << endl;
+    showMenu();
+
+    int menuSelect;
+
+    do {
+        std::cin >> menuSelect;
+        switch(menuSelect) {
+            case 1:
+                //initiate game with Another human player
+                system("clear");
+                pvp();
+                break;
+            case 2:
+                //initiate game with weak computer
+                system("clear");
+               //function for weak comp gamemode;
+                break;
+            case 3:
+                // initiate game with sophisticated computer
+                system("clear");
+                //function for sophisticated ai;
+                break;
+            case 4:
+                //exit game/program
+                system("clear");
+                std::cout << "EXITING..." << std::endl;
+                abort();
+                break;
+            default:    
+                std::cout << "Choose an option 1-4." << std::endl;
+        }
+    } while (menuSelect !=4);
+   
+
+
+    
+    
+    
     
     return 0;
 }
